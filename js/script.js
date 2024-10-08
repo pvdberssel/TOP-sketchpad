2d3c0d81f6284cfff1f2597cad7d91a519c43d98
pvdberssel <pim.bersselaar@live.com>
2024-10-08 13:56:47 +0200

    Add initial grid and event listener

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-console.log("Hello World")
+createGrid();
+
+const grids = document.querySelectorAll('.box');
+
+grids.forEach((grid) => {
+    grid.addEventListener('mouseenter', () => {
+        grid.style.backgroundColor='black'
+    })
+})
+
+
+function createGrid(){
+    const gridContainer = document.querySelector('.grid-container');
+
+    for(j=1;j<16;j++){
+        let container = document.createElement('div');
+        container.className = 'container';
+        gridContainer.appendChild(container);
+        for(i=1;i<16;i++){
+            let box = document.createElement('div');
+            container.appendChild(box);
+            box.className='box';
+        }
+    }
+}
