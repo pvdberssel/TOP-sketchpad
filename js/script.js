550de8f9e2f4df89f9e62937799b6993fdf4a0a3
pvdberssel <pim.bersselaar@live.com>
2024-10-08 15:17:05 +0200

    Add resize button funtionality

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
-createGrid();
+createGrid(20);
+loadGrid();
+const resizeButton = document.querySelector('.resize');
 
-const grids = document.querySelectorAll('.box');
 
-grids.forEach((grid) => {
-    grid.addEventListener('mouseenter', () => {
-        grid.style.backgroundColor='black'
-    })
+resizeButton.addEventListener('click', () => {
+    numberGrid=Number(prompt('How many squares per row and column?'));
+    
+    while(numberGrid < 1 || numberGrid > 100){
+        numberGrid=Number(prompt('Make sure the value is larger than 0 and smaller than 100'));
+    }
+
+    removeGrid();
+    createGrid(numberGrid);
+    loadGrid();
 })
 
-
-function createGrid(){
+function createGrid(amount){
     const gridContainer = document.querySelector('.grid-container');
 
-    for(j=1;j<16;j++){
+    for(j=1;j<=amount;j++){
         let container = document.createElement('div');
         container.className = 'container';
         gridContainer.appendChild(container);
-        for(i=1;i<16;i++){
+        for(i=1;i<=amount;i++){
             let box = document.createElement('div');
             container.appendChild(box);
             box.className='box';
         }
     }
+}
+
+function removeGrid(){
+    const gridContainer = document.querySelector('.grid-container');
+    for(i= gridContainer.childNodes.length - 1; i > -1; i--){
+        gridContainer.removeChild(gridContainer.childNodes[i])
+    }
+}
+
+function loadGrid(){
+    const grids = document.querySelectorAll('.box');
+    
+
+    grids.forEach((grid) => {
+        grid.addEventListener('mouseenter', () => {
+            grid.style.backgroundColor='black'
+        })
+    })
 }
